673bcc048a695d9567271aafce25b76311e4e18c
javier Alejandro <javier_85@hotmail.com>
2020-02-03 20:53:37 -0300

    deploy prueba sql

--- rutasApi/maquinas.js
+++ rutasApi/maquinas.js
@@ -8,7 +8,7 @@ router.get('/', (req,res)=>{
 
 const consulta = new Request();
 
-consulta.query('select * from cliente',(err,dato)=>{ 
+consulta.query('select * from maquinas m inner join tipos_maquina t on m.id_tipos_maquina=t.id',(err,dato)=>{
   !err ? res.json(dato.recordsets) : res.status(403).send(err.originalError.info.message)})
 })
 
