05d09d6cdcdcc3335f5c0d2bc714dcfccc616fc8
javier Alejandro <javier_85@hotmail.com>
2020-02-04 21:59:41 -0300

    cords 3

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
 const express = require('express')
-// const morgan = require('morgan')
-// const path = require('path')
-// require('colors')
 
 const servidor = express()
 
 // middelware
-servidor.use(express.json())
 // servidor.use(function(req, res, next) {
 //     res.header('Access-Control-Allow-Origin', "*");
 //     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
 //     res.header('Access-Control-Allow-Headers', 'Content-Type');
 //     next();
 // })
-servidor.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+servidor.use(express.json())
+servidor.use(express.urlencoded({extended: true}))
 
-servidor.options('*', (req, res) => {
-        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-        res.send();
-    });
+servidor.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', "*"); //<= sólo permitirá conexiones cruzadas desde esta URI
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 });
 
 servidor.use('/api/maquinas',require('./rutasApi/maquinas'))
 servidor.use('/api/usuarios',require('./rutasApi/usuarios'))
 servidor.use('/api/logueo',require('./rutasApi/Logueo'))
 servidor.use('/api/autentificasion',require('./rutasApi/Autentificasion'))
-// servidor.use(morgan('dev'))
-
-
 
 //Settings
 servidor.set('port',process.env.PORT || 5000)
