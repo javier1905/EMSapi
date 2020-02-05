05f796d05a6c668596bb00547e6a94d923e7d7cd
javier Alejandro <javier_85@hotmail.com>
2020-02-04 21:22:15 -0300

    cords 2

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ servidor.use(express.json())
 // })
 servidor.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
+
+servidor.options('*', (req, res) => {
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
 });
 
 servidor.use('/api/maquinas',require('./rutasApi/maquinas'))
