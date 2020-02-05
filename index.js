9adb2d0db21a9aa9df8fdb54810f2e1e2f0e9611
javier Alejandro <javier_85@hotmail.com>
2020-02-04 21:12:53 -0300

    solucion cord

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ const servidor = express()
 
 // middelware
 servidor.use(express.json())
-servidor.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+// servidor.use(function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', "*");
+//     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// })
+servidor.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-})
+});
+
 servidor.use('/api/maquinas',require('./rutasApi/maquinas'))
 servidor.use('/api/usuarios',require('./rutasApi/usuarios'))
 servidor.use('/api/logueo',require('./rutasApi/Logueo'))
