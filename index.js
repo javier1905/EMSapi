4f54a75bf8b4f78f61c22046ec477cd8e2f8fd13
javier sosa <javier_85@hotmail.com>
2020-02-05 07:30:09 -0300

    correccion cors 12

--- index.js
+++ index.js
@@ -11,13 +11,20 @@ servidor.use(cors())
 servidor.use(express.json())
 servidor.use(express.urlencoded({extended: true}))
 
+servidor.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); //<= sólo permitirá conexiones cruzadas desde esta URI
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 servidor.use(morgan('dev'))
 
 servidor.use('/api/maquinas',require('./rutasApi/maquinas'))
 servidor.use('/api/usuarios',require('./rutasApi/usuarios'))
 servidor.use('/api/logueo',require('./rutasApi/Logueo'))
 servidor.use('/api/autentificasion',require('./rutasApi/Autentificasion'))
- 
+
 //Settings
 servidor.set('port',process.env.PORT || 5000)
 
