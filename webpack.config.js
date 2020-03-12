184d87c35a2507bfbeb2b432abaf967a13720cb4
javier Alejandro <javier_85@hotmail.com>
2020-03-12 19:29:58 -0300

    webpack cli dep 3

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const webpackNodeExternals = require('webpack-node-externals')
+const nodemonWebpackPlugin = require( 'nodemon-webpack-plugin' )
+
+module.exports = {
+    mode : 'development' ,
+    target: 'node' ,
+    entry : {
+        app : path.resolve( __dirname , 'index.js' )
+    },
+    output : {
+        path : path.resolve( __dirname , 'dist') ,
+        filename : 'bundle.js'
+    },
+    externals : [ webpackNodeExternals(  ) ] , // avisa a webpack que los librerias nativas no las tranpile 
+    module : {
+            rules : [
+                {
+                    test : /\.js$/ ,
+                    exclude : /node_modules/ ,
+                    loader : 'babel-loader' ,
+                }
+            ],
+    },
+    plugins : [
+        new nodemonWebpackPlugin( {
+            nodeArgs : '--inspect=0.0.0.0:7777' ,
+        } ) ,
+    ],
+    devtool : 'inline-source-map' ,
+}
