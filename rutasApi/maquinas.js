09e42efedce97f31806a863a7ff4838088b395cc
javier Alejandro <javier_85@hotmail.com>
2020-03-20 09:22:36 -0300

    ad op a maq

--- rutasApi/maquinas.js
+++ rutasApi/maquinas.js
@@ -8,7 +8,7 @@ router.get('/', async (req,res)=>{
   const {Request} = require('mssql')
   const consulta = new Request()
   consulta.query(
-    `select m.id as idMaquina, m.nombre as nombreMaquina, m.descripcion as descripcionMaquina
+    `select m.id as idMaquina, m.nombre as nombreMaquina, m.descripcion as descripcionMaquina , id_tipos_maquina as idTipoMaquina
     from maquinas m
     where m.estado = 1`,
     (err,dato)=>{
@@ -24,8 +24,8 @@ router.get('/xoperacion/:idOperacion', async (req,res)=>{
   const consulta = new Request()
   consulta.query(
     `select m.id as idMaquina, m.nombre as nombreMaquina
-    from maquinas m 
-    join tipos_maquina tm on m.id_tipos_maquina = tm.id 
+    from maquinas m
+    join tipos_maquina tm on m.id_tipos_maquina = tm.id
     where m.estado = 1 and tm.id_operacion = ${idOperacion}`,
     (err,dato)=>{
       if(!err){res.json(dato.recordset); cerrarConexion() } else { res.json({mensaje:err.message}); cerrarConexion() }
