29a46877dcdee1c4b2883bcde1d3a24507176795
javier Alejandro <javier_85@hotmail.com>
2020-04-08 10:19:58 -0300

    ruta molde completa

--- rutasApi/moldes.js
+++ rutasApi/moldes.js
@@ -7,7 +7,11 @@ router.get('/',async (req,res)=>{
     await abrirConexion()
     const {Request} = require('mssql')
     const consulta = new Request()
-    consulta.query('select id as idMolde,id_pieza as idPieza, nombre as nombreMolde from moldes where estado = 1',(err,dato)=>{
+    const query = `select m.id as idMolde , m.nombre as nombreMolde , m.id_pieza as idPieza , p.nombre as nombrePieza
+    from moldes m
+    join piezas p on m.id_pieza = p.id
+    where m.estado = 1`
+    consulta.query( query ,(err,dato)=>{
         if(!err){res.json(dato.recordset); cerrarConexion() } else { res.json({mensaje:err.message}); cerrarConexion() }
     })
 })
@@ -21,4 +25,80 @@ router.get('/xpieza/:idPieza', async (req,res)=>{
         if(!err){res.json(dato.recordset); cerrarConexion() } else { res.json({mensaje:err.message}); cerrarConexion() }
     })
 })
+
+
+router.post ( '/insert', async ( req , res ) => {
+    const { nombreMolde , idPieza } = req.body
+    const { abrirConexionPOOL , cerrarConexionPOOL } = require ( '../conexiones/sqlServer' )
+    const { Request , VarChar , Int } = require ( 'mssql' )
+    try {
+        const conexion = await abrirConexionPOOL ( 'insertMolde' )
+        const myRequest = new Request ( conexion )
+        myRequest.input ( 'nombreMolde' , VarChar , nombreMolde )
+        myRequest.input ( 'idPieza' , Int , idPieza )
+        const query = `insert into moldes ( nombre , id_pieza , estado )
+        values
+        ( @nombreMolde , @idPieza , 1 )`
+        const result = await myRequest.query ( query )
+        if ( result ) {
+            cerrarConexionPOOL (  )
+            res.json ( { mensaje : 'Molde Insertado correctamente' , status : 200 } )
+        }
+    }
+    catch ( e ) {
+        cerrarConexionPOOL ( )
+        res.json ( { mensaje : e.message , status : 403 } )
+    }
+})
+router.put ( '/update', async ( req , res ) => {
+    const { idMolde , nombreMolde , idPieza } = req.body
+    const { abrirConexionPOOL , cerrarConexionPOOL } = require ( '../conexiones/sqlServer' )
+    const { Request , VarChar , Int } = require ( 'mssql' )
+    try {
+        const conexion = await abrirConexionPOOL ( 'updateMolde' )
+        const myRequest = new Request ( conexion )
+        myRequest.input ( 'nombreMolde' , VarChar , nombreMolde )
+        myRequest.input ( 'idPieza' , Int , idPieza )
+        myRequest.input ( 'idMolde' , Int , idMolde )
+        const query = `update moldes
+        set
+        nombre = @nombreMolde ,
+        id_pieza = @idPieza
+        where id = @idMolde`
+        const result = await myRequest.query ( query )
+        if ( result ) {
+            cerrarConexionPOOL (  )
+            res.json ( { mensaje : 'Molde actualizado correctamente' , status : 200 } )
+        }
+    }
+    catch ( e ) {
+        cerrarConexionPOOL ( )
+        res.json ( { mensaje : e.message , status : 403 } )
+    }
+})
+router.put ( '/delete', async ( req , res ) => {
+    const { idMolde } = req.body
+    const { abrirConexionPOOL , cerrarConexionPOOL } = require ( '../conexiones/sqlServer' )
+    const { Request , Int } = require ( 'mssql' )
+    try {
+        const conexion = await abrirConexionPOOL ( 'deleteMolde' )
+        const myRequest = new Request ( conexion )
+        myRequest.input ( 'idMolde' , Int , idMolde )
+        const query = `update moldes
+        set
+        estado = 0
+        where id = @idMolde`
+        const result = await myRequest.query ( query )
+        if ( result ) {
+            cerrarConexionPOOL (  )
+            res.json ( { mensaje : 'Molde eliminado correctamente' , status : 200 } )
+        }
+    }
+    catch ( e ) {
+        cerrarConexionPOOL ( )
+        res.json ( { mensaje : e.message , status : 403 } )
+    }
+})
+
+
 module.exports = router
