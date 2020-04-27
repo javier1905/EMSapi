a8df3f31955c485729978d8f4ed4fd14dcc03f71
javier Alejandro <javier_85@hotmail.com>
2020-04-27 12:23:40 -0300

    datos oee granallado listo

--- rutasApi/oee.js
+++ rutasApi/oee.js
@@ -27,4 +27,28 @@ router.post ( '/fundicion' , async ( req , res ) => {
         }
 }  )
 
+router.post ( '/granallado' , async ( req , res ) => {
+    const { idMaquina , idPieza , idMolde , fechaProduccionDesde , fechaProduccionHasta } = req.body
+    const { abrirConexionPOOL , cerrarConexionPOOL } = require ( '../conexiones/sqlServer' )
+    const mssql = require ( 'mssql' )
+        try {
+            const conexion = await abrirConexionPOOL ( 'listaOEEgranallado' )
+            const myReques = new  mssql.Request ( conexion )
+            myReques.input ( 'idMaquina' , mssql.Int , idMaquina )
+            myReques.input ( 'idPieza' , mssql.Int , idPieza )
+            myReques.input ( 'idMolde' , mssql.Int , idMolde )
+            myReques.input ( 'fechaProduccionDesde' , mssql.Date , fechaProduccionDesde )
+            myReques.input ( 'fechaProduccionHasta' , mssql.Date , fechaProduccionHasta )
+            const result = await myReques.execute ( 'pa_datosOEEgra' )
+            if ( result ) {
+                cerrarConexionPOOL (  )
+                res.json ( result.recordset )
+            }
+        }
+        catch ( e ) {
+            cerrarConexionPOOL (  )
+            res.json ( { mensaje : e.message , status : 403 } )
+        }
+}  )
+
 module.exports = router
